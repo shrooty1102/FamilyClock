9d84e1552abc1acb3fc2a457783a54e37c8ff7f5
code <code@legend.root2.in>
2015-03-06 13:57:59 +0530

    new project first commit

---
+++ app/src/main/java/com/themeinnov8/code/weasleyclock/Member.java
@@ -0,0 +1,26 @@
+package com.themeinnov8.code.weasleyclock;
+
+/**
+ * Created by code on 3/5/15.
+ */
+public class Member {
+
+    private String mname;
+    private long phnumber;
+    private String doing;
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+    public String getMname() {
+        return this.mname;
+    }
+
+    public void setPhnumber(long phnumber) {
+        this.phnumber = phnumber;
+    }
+    public long getPhnumber() {
+        return this.phnumber;
+    }
+
+}
---
+++ app/src/main/java/com/themeinnov8/code/weasleyclock/RegistrationActivity.java
@@ -0,0 +1,64 @@
+package com.themeinnov8.code.weasleyclock;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class RegistrationActivity extends ActionBarActivity {
+
+    private EditText username, phnumber;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        initializeComponents();
+
+        setOnClickListeners();
+    }
+
+    private void initializeComponents() {
+        username = (EditText) findViewById(R.id.username);
+        phnumber = (EditText) findViewById(R.id.phnumber);
+        register = (Button) findViewById(R.id.register);
+    }
+
+    private void setOnClickListeners() {
+        // set onClickListener to Register button
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
